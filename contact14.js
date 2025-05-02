45f6c2e61473dc2aaa5d30bb135a7b3bd107c6fa
nihalraza369 <nihalraza369@gmail.com>
2025-05-03 01:42:58 +0500

    project

---
+++ contact14.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("responseText").innerText = "Message Sent Successfully! 🌟";
+    this.reset();
+  });
+  
